2cd85182c53f8d3d35d054b1686b1a56e31957cc
PurpleDerg <sebastian.granda@upr.edu>
2025-11-12 11:47:38 -0400

    ADded config for nemoDD.py

--- Nemo/NemoDD.py
+++ Nemo/NemoDD.py
@@ -13,3 +13,37 @@ from nemo_microservices.data_designer.essentials import (
     ModelConfig,
     InferenceParameters,
 )
+
+
+#Initializing the NeMo Data Designer Client
+NEMO_MICROSERVICES_BASE_URL = "http://localhost:8080"
+
+data_designer_client = NeMoDataDesignerClient(base_url=NEMO_MICROSERVICES_BASE_URL)
+
+
+#Define MODEL CONFIGURATION ------------------------------------------------------
+
+# This name is set in the microservice deployment configuration.
+MODEL_PROVIDER = "nvidiabuild"
+
+# The model ID is from build.nvidia.com.
+MODEL_ID = "nvidia/nvidia-nemotron-nano-9b-v2"
+
+# We choose this alias to be descriptive for our use case.
+MODEL_ALIAS = "nemotron-nano-v2"
+
+# This sets reasoning to False for the nemotron-nano-v2 model.
+SYSTEM_PROMPT = "/no_think"
+
+model_configs = [
+    ModelConfig(
+        alias=MODEL_ALIAS,
+        model=MODEL_ID,
+        provider=MODEL_PROVIDER,
+        inference_parameters=InferenceParameters(
+            temperature=0.5,
+            top_p=1.0,
+            max_tokens=1024,
+        ),
+    )
+]
