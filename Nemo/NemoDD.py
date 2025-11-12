e64a8666379608d96a64c1f7c33d40fb3eb86f97
PurpleDerg <sebastian.granda@upr.edu>
2025-11-12 11:07:19 -0400

    Added Nemo data designer setup python file

---
+++ Nemo/NemoDD.py
@@ -0,0 +1,15 @@
+from getpass import getpass
+
+from nemo_microservices.data_designer.essentials import (
+    CategorySamplerParams,
+    DataDesignerConfigBuilder,
+    LLMTextColumnConfig,
+    NeMoDataDesignerClient,
+    PersonSamplerParams,
+    SamplerColumnConfig,
+    SamplerType,
+    SubcategorySamplerParams,
+    UniformSamplerParams,
+    ModelConfig,
+    InferenceParameters,
+)
